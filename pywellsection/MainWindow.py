3e25441e454eeba8c63c2afba3f4d6c83ac398aa
MSuess <suess@uni-tuebingen.de>
2025-11-19 00:09:55 +0100

    Added stratigraphy table to edit tops and removed some bugs when loading LAS files.

--- pywellsection/MainWindow.py
+++ pywellsection/MainWindow.py
@@ -69,73 +69,6 @@ class MainWindow(QMainWindow):
         # ---- build menu bar ----
         self._create_menubar()
 
-    # def _setup_well_widget_tree(self):
-    #     ### --- Define the Input Tree ###
-    #     self.well_tree = QTreeWidget(self)
-    #     self.well_tree.setHeaderHidden(True)
-    #     self.well_tree.itemChanged.connect(self._on_well_tree_item_changed)
-    #
-    #     # 👇 create the folder item once
-    #     self.well_root_item = QTreeWidgetItem(["Wells"])
-    #     # tristate so checking it checks/unchecks children
-    #     self.well_root_item.setFlags(
-    #         self.well_root_item.flags()
-    #         | Qt.ItemIsUserCheckable
-    #         | Qt.ItemIsTristate
-    #         | Qt.ItemIsSelectable
-    #         | Qt.ItemIsEnabled
-    #     )
-    #     self.well_root_item.setCheckState(0, Qt.Checked)
-    #     self.well_tree.addTopLevelItem(self.well_root_item)
-    #
-    #     self.well_tops_folder = QTreeWidgetItem(["Tops"])
-    #     # tristate so checking it checks/unchecks children
-    #     self.well_tops_folder.setFlags(
-    #         self.well_tops_folder.flags()
-    #         | Qt.ItemIsUserCheckable
-    #         | Qt.ItemIsTristate
-    #         | Qt.ItemIsSelectable
-    #         | Qt.ItemIsEnabled
-    #     )
-    #     self.well_tops_folder.setCheckState(0, Qt.Checked)
-    #     self.well_tree.addTopLevelItem(self.well_tops_folder)
-    #
-    #     self.well_logs_folder = QTreeWidgetItem(["Logs"])
-    #     # tristate so checking it checks/unchecks children
-    #     self.well_logs_folder.setFlags(
-    #         self.well_logs_folder.flags()
-    #         | Qt.ItemIsUserCheckable
-    #         | Qt.ItemIsTristate
-    #         | Qt.ItemIsSelectable
-    #         | Qt.ItemIsEnabled
-    #     )
-    #     self.well_logs_folder.setCheckState(0, Qt.Checked)
-    #     # self.well_root_item.addChild(self.well_logs_folder)
-    #     self.well_tree.addTopLevelItem(self.well_logs_folder)
-    #
-    #     ### Setup the Dock
-    #
-    #     self.well_dock = QDockWidget("Input Data", self)
-    #     self.well_dock.setObjectName("Input")
-    #     self.well_dock.setWidget(self.well_tree)
-    #     self.addDockWidget(Qt.LeftDockWidgetArea, self.well_dock)
-    #
-    #     self.splitDockWidget(self.well_dock, self.dock_panel, Qt.Horizontal)
-    #     self.splitDockWidget(self.dock_panel, self.dock_console, Qt.Vertical)
-    #     self.resizeDocks([self.dock_panel, self.dock_console], [4, 1], Qt.Vertical)
-    #
-    #     self.tabifyDockWidget(self.dock_console, self.dock_commands)
-    #     self.tabifyDockWidget(self.dock_commands, self.dock_logger)
-    #     self.dock_console.raise_()
-    #
-    #     # --- intial build of the well tree
-    #     self._populate_well_tree()
-    #     self._populate_well_tops_tree()
-    #     self._populate_well_log_tree()
-
-    # ------------------------------------------------
-    # MENU BAR SETUP
-    # ------------------------------------------------
     def _create_menubar(self):
         menubar = self.menuBar()
 
@@ -238,23 +171,12 @@ class MainWindow(QMainWindow):
             else:
                 self.all_stratigraphy.update(stratigraphy)
 
-            # for well in wells:
-            #     tops = well.get("tops")
-            #     if tops:
-            #         for top in tops:
-            #             if top in all.stratigraphy:
-            #                 continue
-            #             else:
-            #                 stratigraphy=stratigraphy|tops
-            #     else:
-            #         print("No tops found")
-
             for well in wells:
                 logs = well.get("logs")
                 if logs:
                     for log in logs:
                         if not self.all_logs:
-                            self.all_logs = list(log)
+                            self.all_logs = logs
                         if log in self.all_logs:
                             continue
                         else:
@@ -262,7 +184,7 @@ class MainWindow(QMainWindow):
                 else:
                     print("No logs found")
 
-            self.all_stratigraphy = stratigraphy
+            #self.all_stratigraphy = stratigraphy
 
 
             # populate well tree
@@ -271,7 +193,6 @@ class MainWindow(QMainWindow):
             self._populate_well_log_tree()
             self._populate_well_track_tree()
 
-#            self._rebuild_visible_tops_from_tree()
 
             # ✅ Trigger full redraw
             self.panel.update_panel(tracks, wells, stratigraphy)
@@ -531,7 +452,7 @@ class MainWindow(QMainWindow):
             if not prev_selected:
                 state = Qt.Checked
             else:
-                state = Qt.Checked if name in prev_selected else Qt.Unchecked
+                state = Qt.Checked if strat_name in prev_selected else Qt.Unchecked
             strat_it.setCheckState(0, state)
 
             root.addChild(strat_it)
@@ -928,7 +849,6 @@ class MainWindow(QMainWindow):
         self._populate_well_track_tree()  # tracks folder
         #self._populate_well_log_tree()  # logs folder still valid, but refresh for consistency
 
-
     def _action_delete_track(self):
         """Ask user which track to delete, then call delete_track."""
         if not getattr(self, "all_tracks", None):
@@ -984,5 +904,5 @@ class MainWindow(QMainWindow):
             self.panel.draw_panel()
 
         # 3) refresh "Stratigraphic tops" folder in tree
-        if hasattr(self, "_populate_top_tree"):
-            self._populate_top_tree()
+#        if hasattr(self, "_populate_top_tree"):
+        self._populate_well_tops_tree()
